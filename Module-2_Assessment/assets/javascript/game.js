d7023825252c59b4f17e43bee34a6dd9b9a3d187
Dr Robert McMr <yarnotc@msoe.edu>
2020-03-09 21:59:16 -0500

    finsihed simple set-up for game -Module2

--- Module-2_Assessment/assets/javascript/game.js
+++ Module-2_Assessment/assets/javascript/game.js
@@ -1,3 +1,15 @@
+//#DEFINES
+const MAXIMUM_GUESSES=  10;
+
+//document constants
+const docWinCounter=    document.querySelector("#wins");
+const docLoseCounter=   document.querySelector("#losses");
+const docWord=          document.querySelector("#word");
+const docGuessed=       document.querySelector("#guessed");
+const docRemainCounter= document.querySelector("#remaining");
+const docHealthBar=     document.querySelector("#health");
+const docManaBar=       document.querySelector("#mana");
+
 class word{
     val= "";
     upperVal= "";
@@ -14,47 +26,114 @@ class word{
     isInWord(c){
         return (this.upperVal.indexOf(c.toUpperCase()) != -1);
     }
-    //convert word into a displayable series of underscores
-    toDisplay(guessed){
+    //convert word into a displayable series of underscores/characters
+    toHangman(guessed){
         //create default value for output
-        let out= "<div class=\"row\">";
+        let out= "";
         //iterate through the characters in val
         for(let i= 0; i<this.upperVal.length; i++){
             if(this.upperVal[i] == ' '){
                 //if theres a ' ' create space between underscores recognizable as a space
-                out= out + "</div><div class=\"row\">";
+                out= out + " ";
             }else if(guessed.indexOf(this.upperVal[i]) == -1){
                 //create blank if letter not guessed yet
-                out= out + "<img src=\"assets/images/_.png\" class=\"letter\">";
+                out= out + "_";
             }else{
                 //show letter has been guessed 
-                out= out+ "<img src=\"assets/images/"+this.upperVal[i]+".png\" class=\"letter\">";
+                out= out+ this.upperVal[i];
             }
         }
         //return the output
-        return out+ "</div>";
+        return out;
     }
-
 }
 
 
 //event handler
 function onKeyDown( event ){
-    console.log(event.key);
+    let key= event.key;
     //check if key pressed is letter
-    //set capitalization
-    
+    if(key.length != 1){
+        return;
+    }
+    key= key.toUpperCase()[0];
+    if("ABCDEFGHIJKLMNOPQRSTUVWXYZ".indexOf(key) == -1){
+        return;
+    }
+    //call game on character
+    game.onCharacter(key);
 
 };
+//the font I'm using for my theme is an array of pictures so this is needed
+function stringToHTML(str){
+    //initiallize the output
+    let out= "<div class=\"letters row\">";
+    for(i=0; i<str.length; i++){
+        if(str[i] == " "){
+            //if letter is a " " new line
+            out= out + "</div><div class=\"letters row\">";
+        }else{
+            //otherwise add the image for the character
+            out= out+ "<img src=\"assets/images/" 
+                + str[i] + ".png\" class=\"letter\">";
+        }
+    }
+    //return the output with the ending
+    return out+ "</div>";
+}
+
 //game
 const game = {
     //variables
+    currentWord:    new word("Hollow Knight","Hollow Knight is an indie game."),
+    wins:           0,
+    losses:         0,
+    guessRemain:    MAXIMUM_GUESSES,
+    wrongLetters:   "",
+    rightLetters:   "",
+    //update things that change when you're wrong:
+    //healthbar, remaining counter, and guessed letters
+    updateWrong(){
+        //iterate 0->MAX_GUESSES
+        let innerHTML= "";
+        for(i= 0; i<MAXIMUM_GUESSES; i++){
+            //add a full heart if i < remaining guesses and empty otherwise
+            innerHTML= innerHTML + "<img src=\"assets/images/";
+            if(i > this.guessRemain){
+                innerHTML= innerHTML + "empty";
+            }
+            innerHTML= innerHTML + "heart.png\" class=\"hp\">";
+        }
+        //update the document
+        docHealthBar.innerHTML= innerHTML;
+        //update remaining guesses counter
+        docRemainCounter.innerText= " " + this.guessRemain + "/" + MAXIMUM_GUESSES;
+        //guessed letters
+        //convert string to pictures
+        //update guessed letters HTML
+        docGuessed.innerHTML= stringToHTML(this.wrongLetters);
+    },
+    //update things that change when you're right:
+    //the word
+    updateRight(){
+        //convert word to displayable series of underscores/characters
+        let str= this.currentWord.toHangman(this.rightLetters);
+        //convert word to pictures
+        //update word display
+        docWord.innerHTML= stringToHTML(str);
+    },
 
     //(re)start
     start : function() {
         //set all values to default
+        this.wrongLetters=  "",
+        this.rightLetters=  "",
+        this.guessRemain= MAXIMUM_GUESSES,
+
         //pick a random word
         //set game area to default
+        this.updateWrong();
+        this.updateRight();
     },
     //on win or loss
     onWinLose : function(hasWon){
@@ -69,8 +148,9 @@ const game = {
         //show description
     },
     //when a character is pressed
-//check if letter is in word
-    onCharacter : function(){
+    onCharacter : function(key){
+        console.log(key);
+        //check if character is in word
         if (true){
             //update word
             //check if word is complete
@@ -87,6 +167,7 @@ const game = {
 
         }
     }
+
 };
 //on startup
 
@@ -95,3 +176,4 @@ const game = {
 //(re)start game
 //add event listener
 window.addEventListener("keydown", onKeyDown);
+game.start();
